a88f433c3c0f1fbf921b8cfe9381478f29c54cbf
Jvviercz <129097963+Jvviercz@users.noreply.github.com>
2023-07-05 21:01:52 -0400

    Initial commit

---
+++ desafio6.2.js
@@ -0,0 +1,12 @@
+
+
+function mostrarOperaciones(){
+    var numero1 = parseInt(document.getElementById('numero1').value);
+    var numero2 = parseInt(document.getElementById('numero2').value);
+    var resultado = numero1 + "*" + numero2 + "=" + (numero1 * numero2) + "<br>" +
+    numero1 + "/" + numero2 + "=" + (numero1 / numero2) + "<br>" +
+    numero1 + "+" + numero2 + "=" + (numero1 + numero2) + "<br>" +
+    numero1 + "-" + numero2 + "=" + (numero1 - numero2);
+
+    document.getElementById('resultados').innerHTML = resultado;
+}
